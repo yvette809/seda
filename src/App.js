df685dd5ec58a0dbbeb7284666d78c4ff803ced7
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-12-28 23:33:08 +0100

    included react/router/dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@
 import React from 'react'
 import Navigation from './components/Navigation'
 import Home from './pages/Home'
+import Vision from './pages/Vision'
+import Mission from './pages/Mission'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Background from './pages/Background'
+import Sponsors from './pages/Sponsors'
 
 const App = () => {
   return (
-    <div>
-      <Navigation/>
-      <Home/>
-      </div>
+    <BrowserRouter>
+     <Navigation/>
+    <Routes>
+    <Route path="/" element={<Navigation />}/>  
+    <Route index element={<Home />}/>      
+    <Route path="vision" element={<Vision/>} />
+    <Route path="mission" element={<Mission/>} />
+    <Route path="background" element={<Background/>} />
+    <Route path="sponsors" element={<Sponsors/>} />
+       {/*    <Route path="*" element={<NoPage />} /> */}
+       
+      </Routes>
+      </BrowserRouter>
   )
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,53 +3,56 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo-01.jpg'
+import { Link,Outlet } from 'react-router-dom';
 
 function Navigation() {
   return (
+    <>
     <Navbar expand="lg" className="main-nav" >
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="seda logo" className="logo" /></Navbar.Brand>
+        <Link to="/"><Navbar.Brand ><img src={logo} alt="seda logo" className="logo" /></Navbar.Brand></Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="ABOUT US" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Our Vision</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Our Mission
+              <NavDropdown.Item> <Link to="/vision">Our Vision</Link></NavDropdown.Item>
+              <NavDropdown.Item>
+              <Link to="/mission">Our Mission</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Our Background</NavDropdown.Item>
+              <NavDropdown.Item ><Link to="/background">Our Background</Link></NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-              Our Sponsors/Partners
+              <NavDropdown.Item >
+              <Link to="/sponsors">Our Sponsors/Partners</Link>
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="WHAT WE DO" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">We Educate</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-               We Cultivate
+              <NavDropdown.Item ><Link to="/education">We Educate</Link></NavDropdown.Item>
+              <NavDropdown.Item>
+              <Link to="/cultivation">We Cultivate</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">We Take Climate Action</NavDropdown.Item>
+              <NavDropdown.Item><Link to="/climate-action">We Take climate Action</Link></NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                We Advocate
+              <NavDropdown.Item >
+              <Link to="/advocate">We Advocate</Link>
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="GET INVOLVED" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Donate</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-              Volunteer
+              <NavDropdown.Item ><Link to="/donation">Donation</Link></NavDropdown.Item>
+              <NavDropdown.Item >
+              <Link to="/volunteer">Volunteer</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">We Take Climate Action</NavDropdown.Item>
+          
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-               News & Events
+              <NavDropdown.Item >
+              <Link to="/news">News and Events</Link>
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#link">HOME</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <Outlet/>
+    </>
   );
 }
 
