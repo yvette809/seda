03d304202c5446a47cde53ab273ea0bcb53e74f6
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-12-30 00:24:50 +0100

    changed the image and edited some info

--- src/pages/Background.jsx
+++ src/pages/Background.jsx
@@ -3,23 +3,50 @@ import Hero from '../components/Hero'
 import image1 from '../assets/about-us.jpg'
 import image2 from '../assets/about-us2.jpg'
 import DonationBanner from '../components/DonationBanner'
+import {Link} from 'react-router-dom'
 
 const Background = () => {
   return (
     <>
-   <Hero title="Who We are" description="SEDA, initiated in early 2019, obtained legal recognition as a not-for-profit, non-governmental, and non-political organization with registration number 63/E.29/1111/VOL.8/ALPAS in 2021. This establishment occurred under the auspices of Cameroon's Ministry of Territorial Administration. Spearheaded by a team of young leaders led by the visionary founding president, Ache William, SEDA emerged from his early university years where he cultivated a profound commitment to addressing the escalating challenges facing human well-being and settlement, particularly focusing on women and youth in rural communities.
+   <Hero
+  title="Who We are"
+  description={
+    <>
+      <p>
+        SEDA was initiated early 2019 and legally established as a not-for-profit
+        non-governmental and non-political organization with registration number
+        63/E.29/1111/VOL.8/ALPAS under the umbrella of Cameroon's Ministry of
+        Territorial Administration in 2021.
+      </p>
+      <p>
+        Founded by a team of youths led by the founding president{' '}
+        <Link to="https://www.linkedin.com/in/ache-william-275bab205" className="founder">
+          Ache William
+        </Link>
+        , who, in his early years at the university, developed a deep passion for
+        addressing the growing challenges of human well-being and related
+        settlement, with a particular focus on women and youth in rural
+        communities.
+      </p>
+      {/* Add more paragraphs or HTML tags as needed */}
+    </>
+  }
+  image1={image1}
+  image2={image2}
+/>
 
-   Since its inception, SEDA has steadfastly directed its efforts towards alleviating illiteracy, poverty, hunger, and the impacts of climate change. Through the strategic empowerment of women and youth in grassroots communities, incorporating transferable life skills, tangible resources, and mentoring/coaching, with a special emphasis on Climate Smart Agriculture, SEDA aims to holistically tackle the interconnected issues plaguing humanity. The organization firmly believes that by addressing these challenges simultaneously, without compromising the resolution of any one issue, sustainable solutions can be achieved." image1={image1} image2={image2}/>
 
 <DonationBanner title1="Please Join Us To Give Our Community A Better Life " title2='Just Like Muhammed Ali said "Service To Others Is The Rent You Pay Here On Earth"' action="Let's Donate"/>
 <div className="container mb-3">
    <p className='mt-4'><strong>Our Core Objectives are:</strong></p>
    <ul className="objectives">
-    <li>To empower and engage communities in biodiversity conservation, afforestation and reforestation and degraded land restoration</li>
-    <li>We promote innovative strategies for community development and poverty alleviation</li>
-    <li>We promote the sustainable management of natural resources</li>
-    <li>To advocate for and promote access and right of ownership of land by women, indigenous people and small scale farmers</li>
-    <li>To promote quality education to youths and vulnarable persons</li>
+    <li>To fight against climate change by ensuring the protection, preservation, rehabilitation, restoration and efficient management of water tables, wetlands, water catchments etc as well as other fresh water sources and habitats for fauna and flora</li>
+    <li>To promote sustainable agricultural practices, motivate youths and women in grassroots Communities to be engaged in Agriculture as a way to fight poverty and unemployment, thereby ensuring food security and reducing hunger, malnutrition and promotion of Entrepreneurial and leadership skills for sustainable livelihoods</li>
+    <li>To promote empowerment and social outreach programs intended to train or mentor the underprivileged ir order for them to acquire skills in domains such as Agriculture and other social development initiatives that can help improve their social and economic well-being</li>
+    <li>To Encourage Rural Communities to preserve and protect their biodiversity and support them in planting of environmentally friendly trees</li>
+    <li>To Curb youth unemployment by promoting social entrepreneurship</li>
+    <li>To promote the practice of Eco-friendly Agricultural activities, mitigating the effects of land degradation and ensuring healthy dieting habits</li>
+    <li>To enhance Sustainable Development by educating the local population on the need to protect the environment and better manage their natural resources so as to prevent the adverse effects of climate change and nature degradation</li>
 
    </ul>
    </div>
--- src/pages/Vision.jsx
+++ src/pages/Vision.jsx
@@ -7,9 +7,9 @@ import { FaQuoteRight } from "react-icons/fa6";
 const Vision = () => {
   return (
     <>
-    <Hero title= "Our Vision" image1={image1} description="We have etched our vision as a blueprint on the tablets of our hearts. As we aspire to foster just, peaceful, and sustainable communities worldwide, where every individual can not only exist but flourish, we firmly acknowledge that transitioning from mere rhetoric to tangible reality necessitates the concerted efforts of individuals and organizations alike. Our overarching vision is thus:"/>
+    <Hero title= "Our Vision" image1={image1} description="We have our Vision as a Blue Print on the tablets of our Hearts. While Thriving for a just,peaceful and sustainable communities across the globe where everyone can live and thrive, we strongly believe that to call it out from rhetoric to reality requires the collaborative efforts of individuals and organisations. Our main Vision is therefore:"/>
 
-    <p className='my-4  vision'><em><FaQuoteRight className='fa-right mx-3'/>Establish a sustainable ecosystem<br/> with an equitable distribution of resources <br/> to enhance livelihoods across all parts and wholes</em></p>
+    <p className='my-4  vision'><em><FaQuoteRight className='fa-right mx-3'/> Sustainable ecosystems<br/> with Equitable Distribution of resources for <br/> improved livelihoods within all parts and wholes</em></p>
     
     </>
     
