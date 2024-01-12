ab2ef86882b529f77542bdb56b0f06a72bdf7621
yvette809 <60625477+yvette809@users.noreply.github.com>
2024-01-12 01:59:55 +0100

    vision page made responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ({image1, image2,title, description}) => {
            {image2 && <img src={image2} alt="background-img" className='img-2' />}
         </div>
         <div className="hero-description col-lg-6">
-            <h1>{title}</h1>
+            <h1 className='title'>{title}</h1>
             <p className='text-muted'>{description}</p>
         </div>
            
